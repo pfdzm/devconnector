c375501f8592c1d3543f1e09335531e1ad8ed545
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-04-01 21:13:27 +0200

    set up basic routes

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
+// connect to DB
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
