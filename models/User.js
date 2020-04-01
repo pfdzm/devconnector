f0f42740a52afd022152ecb953e5269b73574cc9
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-04-01 21:41:02 +0200

    fix some autocomplete typos

--- models/User.js
+++ models/User.js
@@ -3,12 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: TextTrackCue
+    required: true
   },
   email: {
     type: String,
-    required: true,
-    unique: TextTrackCue
+    required: true
   },
   password: {
     type: String,
